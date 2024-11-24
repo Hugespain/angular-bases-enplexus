541d3d378e1c6574cd59a088bcb3059cd59d19b9
Hugespain <diegosilva3795@gmail.com>
2024-11-24 16:33:49 +0100

    Ejercicio del video 69 resulto y comprendido

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-add-character',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AddCharacterComponent {
 
+  @Output()
+  onNewCharacter: EventEmitter<Character>= new EventEmitter();
+
+  public character: Character= {
+    name: '',
+    power:0,
+  };
+
+  emitCharacter():void{
+    console.log(this.character);
+    if(this.character.name.length ===0 ) return;
+
+    this.onNewCharacter.emit(this.character);
+
+  }
 }
